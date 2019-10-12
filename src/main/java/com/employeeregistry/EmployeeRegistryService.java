f2977185a16fb4e044888d81d46275333ae5198b
chandra-prakash-reddy <41863702+chandra-prakash-reddy@users.noreply.github.com>
2019-10-12 23:09:06 +0530

    modified main

--- src/main/java/com/employeeregistry/EmployeeRegistryService.java
+++ src/main/java/com/employeeregistry/EmployeeRegistryService.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeRegistryService {
 	public static void main(String[] args) throws Exception {
-		throw new Exception("hello");
+		throw new Exception("hello"); 
 		SpringApplication.run(EmployeeRegistryService.class, args);
 	}
 }
