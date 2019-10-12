bd6e08896c175dc621589f53994b7e3aab81b0c4
chandra-prakash-reddy <41863702+chandra-prakash-reddy@users.noreply.github.com>
2019-10-12 23:14:56 +0530

    added throws exception

--- src/main/java/com/employeeregistry/EmployeeRegistryService.java
+++ src/main/java/com/employeeregistry/EmployeeRegistryService.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeRegistryService {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception  {
 		throw new Exception("hello");
 		SpringApplication.run(EmployeeRegistryService.class, args);
 	}
